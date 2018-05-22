3ae0b13153f330230a1c82f1cb815bebe88ee5f5
Topias Teittinen <topias_t@hotmail.com>
2018-05-22 18:44:10 +0300

    basic index page

--- application/views.py
+++ application/views.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from application import app
+from application.messages.models import Message
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    messagelist = Message.query.distinct(Message.thread_id)
+    messageset = set()
+    for m in messagelist:
+        messageset.add(m.thread_id)
+
+    return render_template("index.html", threads = messageset)
