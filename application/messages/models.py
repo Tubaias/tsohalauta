5ea978b77e5e8033eaa135cee04f43e4d4a5555a
Topias Teittinen <topias_t@hotmail.com>
2018-06-05 16:09:45 +0300

    Base class for database models

--- application/messages/models.py
+++ application/messages/models.py
@@ -1,10 +1,7 @@
 from application import db
+from application.models import Base
 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate = db.func.current_timestamp())
-    
+class Message(Base):
     name = db.Column(db.String(144), default="Anonymous")
     text = db.Column(db.String(1000), nullable=False)
     thread_id = db.Column(db.Integer, nullable=False)
